2b96f636ed5d82b4c07a8f2d007fbc9781318b0a
chibieee <gregneo21@gmail.com>
2016-08-19 00:27:13 -0400

    Part 6 - Routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -7,6 +7,12 @@ export class HeroService {
     getHeroes() {
         return Promise.resolve(HEROES);
     }
+
+    getHero(id: number) {
+        return this.getHeroes()
+                    .then(heroes => heroes.find(hero => hero.id === id));
+        }
+
     constructor() { }
 
 }
